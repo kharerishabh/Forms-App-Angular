ac0d7ef703c0d62725176c30d3409c7e02197303
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-13 22:40:47 +0530

    using Form data and rendering on the screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'pet'
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -38,7 +46,11 @@ export class AppComponent {
 
   //Another way of Accessing Form using ViewChild.
   onSubmit () {
-    console.log(this.signupForm);
-    
+    this.submitted = true
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer
+    this.user.gender = this.signupForm.value.gender;  
   }
 }
